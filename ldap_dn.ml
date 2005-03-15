c2f5ab3613b8cbe33d78bc27062ee4b19afd084b
Eric Stokes <letaris@me.com>
2005-03-15 02:03:07 +0000

    working on to_string. Still a few bugs I need to stamp out

--- ldap_dn.ml
+++ ldap_dn.ml
@@ -21,24 +21,99 @@
 
 open Ldap_types
 open Ldap_dnparser
+open Printf
 
 let of_string dn_string = 
   try Ldap_dnparser.dn dn_string
   with Parsing.Parser_error | Failure _ ->
     raise (Invalid_dn "parse error")
 
+let hexpair_of_char c = 
+  let hexify i = 
+    match i with
+	0 -> '0'
+      | 1 -> '1'
+      | 2 -> '2'
+      | 3 -> '3'
+      | 4 -> '4'
+      | 5 -> '5'
+      | 6 -> '6'
+      | 7 -> '7'
+      | 8 -> '8'
+      | 9 -> '9'
+      | 10 -> 'a'
+      | 11 -> 'b'
+      | 12 -> 'c'
+      | 13 -> 'd'
+      | 14 -> 'e'
+      | 15 -> 'f'
+      | n -> raise (Invalid_argument ("invalid hex digit: " ^ (string_of_int n)))
+  in
+  let i = int_of_char c in
+  let buf = String.create 2 in
+    buf.[0] <- hexify ((lsr) i 4);
+    buf.[1] <- hexify ((land) i 0b0000_1111);
+    buf
+
 let escape_value valu = 
   let strm = Stream.of_string valu in
   let buf = Buffer.create ((String.length valu) + 10) in
   let rec escape strm buf = 
-    match Stream.next strm with
-	(',' | '=' | '+' | '<' | '>' | '#' | ';' | '\\' | '"') as c ->
-	  Buffer.add_char buf '\\';
-	  Buffer.add_char buf c;
+    try
+      match Stream.next strm with
+	  (',' | '=' | '+' | '<' | '>' | '#' | ';' | '\\' | '"') as c ->
+	    Buffer.add_char buf '\\';
+	    Buffer.add_char buf c;
 	  escape strm buf
-      | c -> Buffer.add_char buf c;escape strm buf
+	| ' ' -> 
+	    if Stream.peek strm = None then begin
+	      Buffer.add_string buf "\\ ";
+	      escape strm buf
+	    end
+	    else begin
+	      Buffer.add_char buf ' ';
+	      escape strm buf
+	    end
+	| c ->
+	    if (int_of_char c) < (int_of_char ' ') || 
+	       (int_of_char c) > (int_of_char '~') 
+	    then begin
+	      Buffer.add_string buf ("\\" ^ (hexpair_of_char c));
+	      escape strm buf
+	    end
+	    else begin
+	      Buffer.add_char buf c;escape strm buf
+	    end
+    with Stream.Failure -> Buffer.contents buf
+  in
+    match Stream.peek strm with
+	Some ' ' -> 
+	  Buffer.add_string buf "\\ ";
+	  Stream.junk strm;
+	  escape strm buf
+      | Some c -> escape strm buf
+      | None -> ""
 
 let to_string dn = 
-  List.fold_left
-    (fun s {attr_type=attr;attr_vals=vals} ->
-       
+  let dn_to_strcomponents dn = 
+    List.map
+      (fun {attr_type=attr;attr_vals=vals} ->
+	 let rec string_values s attr vals =
+	   match vals with
+	       valu :: [] -> sprintf "%s%s=%s" s attr (escape_value valu)
+	     | valu :: tl -> string_values (sprintf "%s%s=%s+" s attr (escape_value valu)) attr tl
+	     | [] -> s
+	 in
+	   if List.length vals = 0 then
+	     raise (Invalid_dn ("no attribute value specified for attribute: " ^ attr))
+	   else
+	     string_values "" attr vals)
+      dn
+  in
+  let rec components_to_dn s comps =
+    match comps with
+	comp :: [] -> sprintf "%s%s" s comp
+      | comp :: tl -> components_to_dn (sprintf "%s%s," s comp) tl
+      | [] -> s
+  in
+    components_to_dn "" (dn_to_strcomponents dn)
