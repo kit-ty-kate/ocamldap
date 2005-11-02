a8e2d2928c1a0f8edc7241c7e61a5945a4d5a969
Eric Stokes <letaris@me.com>
2005-11-02 22:15:58 +0000

    adding syntaxes

---
+++ ldap_syntax.ml
@@ -0,0 +1,72 @@
+(* 1.3.6.1.4.1.1466.115.121.1.3 DESC 'Attribute Type Description' *)
+let attribute_type_description_syntax v = ()
+  
+(* 1.3.6.1.4.1.1466.115.121.1.5 DESC 'Binary' *)
+let binary_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.6 DESC 'Bit String' *)
+let bitstring_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.7 DESC 'Boolean' *)
+let boolean_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.8 DESC 'Certificate' *)
+let certificate_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.9 DESC 'Certificate List' *)
+let certificate_list_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.10 DESC 'Certificate Pair' *)
+let certificate_pair_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.11 DESC 'Country String' *)
+let country_string_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.12 DESC 'DN' *)
+let dn_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.15 DESC 'Directory String' *)
+let directory_string_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.22 DESC 'Facsimile Telephone Number' *)
+let facsimile_telephone_number_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.23 DESC 'Fax' *)
+let fax_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.24 DESC 'Generalized Time' *)
+let generalized_time_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.26 DESC 'IA5 String' *)
+let ia5_string_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.27 DESC 'INTEGER' *)
+let integer_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.28 DESC 'JPEG' *)
+let jpeg_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.30 DESC 'Matching Rule Description' *)
+let matching_rule_description_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.31 DESC 'Matching Rule Use Description' *)
+let matching_rule_use_description_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.33 DESC 'MHS OR Address' *)
+let mhs_or_address_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.34 DESC 'Name And Optional UID' *)
+let name_and_optional_uid v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.35 DESC 'Name Form Description' *)
+let name_form_description_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.36 DESC 'Numeric String' *)
+let numeric_string_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.37 DESC 'Object Class Description' *)
+let object_class_description_syntax v = ()
+
+(* 1.3.6.1.4.1.1466.115.121.1.38 DESC 'OID' *)
+let oid_syntax v = ()
+
