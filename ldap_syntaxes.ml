6c4fb85cbdd9cb3f27822b2f496b2aebf274c249
Eric Stokes <letaris@me.com>
2005-11-03 23:29:34 +0000

    added a map of syntaxes

--- ldap_syntaxes.ml
+++ ldap_syntaxes.ml
@@ -1,3 +1,5 @@
+open Ldap_schema
+
 (* 1.3.6.1.4.1.1466.115.121.1.3 DESC 'Attribute Type Description' *)
 let attribute_type_description_syntax v = ()
   
@@ -94,3 +96,39 @@ let ldap_syntax_description v = ()
 (* 1.3.6.1.4.1.1466.115.121.1.17 DESC 'DIT Structure Rule Description' *)
 let dit_structure_rule_description_syntax v = ()
 
+let syntaxes = 
+  List.fold_left
+    (fun m (key, value) -> Oidmap.add key value m)
+    Oidmap.empty
+    [(Oid.of_string "1.3.6.1.4.1.1466.115.121.1.3", attribute_type_description_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.5", binary_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.6", bitstring_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.7", boolean_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.8", certificate_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.9", certificate_list_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.10", certificate_pair_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.11", country_string_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.12", dn_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.15", directory_string_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.22", facsimile_telephone_number_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.23", fax_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.24", generalized_time_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.26", ia5_string_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.27", integer_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.28", jpeg_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.30", matching_rule_description_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.31", matching_rule_use_description_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.33", mhs_or_address_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.34", name_and_optional_uid);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.35", name_form_description_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.36", numeric_string_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.37", object_class_description_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.38", oid_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.39", other_mailbox_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.41", postal_address_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.43", presentation_address_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.44", printable_string_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.50", telephone_number_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.53", utc_time_syntax);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.54", ldap_syntax_description);
+     (Oid.of_string "1.3.6.1.4.1.1466.115.121.1.17", dit_structure_rule_description_syntax)]
