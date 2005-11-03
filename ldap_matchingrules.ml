834e75225c5a2858f12e01a30684f8a554cdf4f9
Eric Stokes <letaris@me.com>
2005-11-03 19:22:11 +0000

    matching rules

---
+++ ldap_matchingrules.ml
@@ -0,0 +1,27 @@
+(* equality matching rules *)
+
+(* 2.5.13.0 NAME 'objectIdentifierMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.38 *)
+let object_identifier_equality_match v1 v2 = Pervasives.compare v1 v2
+
+(* 2.5.13.1 NAME 'distinguishedNameMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.12 *)
+let distinguished_name_equality_match v1 v2 = Pervasives.compare v1 v2
+
+(* 2.5.13.2 NAME 'caseIgnoreMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.15 *)
+let case_ignore_equality_match v1 v2 = 
+  Pervasives.compare (String.lowercase v1) (String.lowercase v2)
+
+(* 2.5.13.8 NAME 'numericStringMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.36 *)
+let numeric_string_equality_match v1 v2 = Pervasives.compare v1 v2
+
+(* 2.5.13.11 NAME 'caseIgnoreListMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.41 *)
+let case_ignore_list_equality_match = case_ignore_equality_match
+
+(* 2.5.13.14 NAME 'integerMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.27 *)
+let integer_equality_match v1 v2 = Pervasives.compare v1 v2
+
+(* 2.5.13.16 NAME 'bitStringMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.6 *)
+let bit_string_equality_match v1 v2 = Pervasives.compare v1 v2
+
+(* 2.5.13.20 NAME 'telephoneNumberMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.50 *)
+let telephone_number_match v1 v2 = Pervasives.compare v1 v2
+
