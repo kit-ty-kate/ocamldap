a3fbd4b7c431e780b169bed0298d5ab75bd3672a
Eric Stokes <letaris@me.com>
2005-11-04 23:38:47 +0000

    the map compiles

--- ldap_matchingrules.ml
+++ ldap_matchingrules.ml
@@ -1,14 +1,51 @@
 open Ldap_types
 open Ldap_schema
 
-type ('a) t = {
-  add: (string -> 'a -> 'a);
-  mem: (string -> 'a -> bool);
-  remove: (string -> 'a -> 'a);
-  empty: 'a;
-  elements: ('a -> string list);
-  compare: (string -> string -> int)
-}
+exception Value_exists
+exception Value_does_not_exist
+
+class type attribute_t =
+object
+  method add: ?idempotent:bool -> string -> unit
+  method delete: ?idempotent:bool -> string -> unit
+  method replace: string list -> unit
+  method exists: string -> bool
+  method values: string list
+end
+
+class ['a] attribute 
+  (add: string -> 'a -> 'a) 
+  (mem: string -> 'a -> bool)
+  (remove: string -> 'a -> 'a)
+  (empty: 'a)
+  (elements: 'a -> string list)
+  (syntax: string -> unit) =
+object
+  val mutable store = empty
+  method add ?(idempotent=false) v =
+    syntax v;
+    if idempotent then
+      store <- add v store
+    else if mem v store then
+      raise Value_exists
+    else
+      store <- add v store
+  method delete ?(idempotent=false) v =
+    if idempotent then
+      store <- remove v store
+    else if not (mem v store) then
+      raise Value_does_not_exist
+    else
+      store <- remove v store
+  method replace vals =
+    store <- 
+      (List.fold_left
+	 (fun s v -> syntax v;add v s)
+	 empty
+	 vals)
+  method exists v = mem v store
+  method values = elements store
+end
 
 (* equality matching rules *)
 
@@ -21,14 +58,14 @@ module ObjectIdentifierMatch = Set.Make
      let compare = object_identifier_equality_match
    end)
 
-let object_identifier_set = {
-  add=ObjectIdentifierMatch.add;
-  mem=ObjectIdentifierMatch.mem;
-  remove=ObjectIdentifierMatch.remove;
-  empty=ObjectIdentifierMatch.empty;
-  elements=ObjectIdentifierMatch.elements;
-  compare=object_identifier_equality_match
-}
+let new_object_identifier_equality_set syntax =
+  (new attribute
+     ObjectIdentifierMatch.add
+     ObjectIdentifierMatch.mem
+     ObjectIdentifierMatch.remove
+     ObjectIdentifierMatch.empty
+     ObjectIdentifierMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.1 NAME 'distinguishedNameMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.12 *)
 let distinguished_name_equality_match v1 v2 = String.compare v1 v2
@@ -39,14 +76,14 @@ module DistinguishedNameMatch = Set.Make
      let compare = distinguished_name_equality_match
    end)
 
-let distinguished_name_equality_set = {
-  add=DistinguishedNameMatch.add;
-  mem=DistinguishedNameMatch.mem;
-  remove=DistinguishedNameMatch.remove;
-  empty=DistinguishedNameMatch.empty;
-  elements=DistinguishedNameMatch.elements;
-  compare=distinguished_name_equality_match
-}
+let new_distinguished_name_equality_set syntax =
+  (new attribute
+     DistinguishedNameMatch.add
+     DistinguishedNameMatch.mem
+     DistinguishedNameMatch.remove
+     DistinguishedNameMatch.empty
+     DistinguishedNameMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.8 NAME 'numericStringMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.36 *)
 let numeric_string_equality_match v1 v2 = String.compare v1 v2
@@ -57,14 +94,14 @@ module NumericStringMatch = Set.Make
      let compare = numeric_string_equality_match
    end)
 
-let numeric_string_equality_set = {
-  add=NumericStringMatch.add;
-  mem=NumericStringMatch.mem;
-  remove=NumericStringMatch.remove;
-  empty=NumericStringMatch.empty;
-  elements=NumericStringMatch.elements;
-  compare=numeric_string_equality_match
-}
+let new_numeric_string_equality_set syntax =
+  (new attribute
+     NumericStringMatch.add
+     NumericStringMatch.mem
+     NumericStringMatch.remove
+     NumericStringMatch.empty
+     NumericStringMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.14 NAME 'integerMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.27 *)
 let integer_equality_match v1 v2 = String.compare v1 v2
@@ -75,14 +112,14 @@ module IntegerMatch = Set.Make
      let compare = integer_equality_match
    end)
 
-let integer_equality_set = {
-  add=IntegerMatch.add;
-  mem=IntegerMatch.mem;
-  remove=IntegerMatch.remove;
-  empty=IntegerMatch.empty;
-  elements=IntegerMatch.elements;
-  compare=integer_equality_match
-}
+let new_integer_equality_set syntax =
+  (new attribute
+     IntegerMatch.add
+     IntegerMatch.mem
+     IntegerMatch.remove
+     IntegerMatch.empty
+     IntegerMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.16 NAME 'bitStringMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.6 *)
 let bit_string_equality_match v1 v2 = String.compare v1 v2
@@ -93,14 +130,14 @@ module BitStringMatch = Set.Make
      let compare = bit_string_equality_match
    end)
 
-let bit_string_equality_set = {
-  add=BitStringMatch.add;
-  mem=BitStringMatch.mem;
-  remove=BitStringMatch.remove;
-  empty=BitStringMatch.empty;
-  elements=BitStringMatch.elements;
-  compare=bit_string_equality_match
-}
+let new_bit_string_equality_set syntax =
+  (new attribute
+     BitStringMatch.add
+     BitStringMatch.mem
+     BitStringMatch.remove
+     BitStringMatch.empty
+     BitStringMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.22 NAME 'presentationAddressMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.43 *)
 let presentation_address_equality_match v1 v2 = String.compare v1 v2
@@ -111,14 +148,14 @@ module PresentationAddressMatch = Set.Make
      let compare = presentation_address_equality_match
    end)
 
-let presentation_address_equality_set = {
-  add=PresentationAddressMatch.add;
-  mem=PresentationAddressMatch.mem;
-  remove=PresentationAddressMatch.remove;
-  empty=PresentationAddressMatch.empty;
-  elements=PresentationAddressMatch.elements;
-  compare=presentation_address_equality_match
-}
+let new_presentation_address_equality_set syntax =
+  (new attribute
+     PresentationAddressMatch.add
+     PresentationAddressMatch.mem
+     PresentationAddressMatch.remove
+     PresentationAddressMatch.empty
+     PresentationAddressMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.23 NAME 'uniqueMemberMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.34 *)
 let unique_member_equality_match v1 v2 = String.compare v1 v2
@@ -129,14 +166,14 @@ module UniqueMemberMatch = Set.Make
      let compare = unique_member_equality_match
    end)
 
-let unique_member_equality_set = {
-  add=UniqueMemberMatch.add;
-  mem=UniqueMemberMatch.mem;
-  remove=UniqueMemberMatch.remove;
-  empty=UniqueMemberMatch.empty;
-  elements=UniqueMemberMatch.elements;
-  compare=unique_member_equality_match
-}
+let new_unique_member_equality_set syntax =
+  (new attribute
+     UniqueMemberMatch.add
+     UniqueMemberMatch.mem
+     UniqueMemberMatch.remove
+     UniqueMemberMatch.empty
+     UniqueMemberMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.24 NAME 'protocolInformationMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.42 *)
 let protocol_information_equality_match v1 v2 = String.compare v1 v2
@@ -147,14 +184,14 @@ module ProtocolInformationMatch = Set.Make
      let compare = protocol_information_equality_match
    end)
 
-let protocol_information_equality_set = {
-  add=ProtocolInformationMatch.add;
-  mem=ProtocolInformationMatch.mem;
-  remove=ProtocolInformationMatch.remove;
-  empty=ProtocolInformationMatch.empty;
-  elements=ProtocolInformationMatch.elements;
-  compare=protocol_information_equality_match
-}
+let new_protocol_information_equality_set syntax =
+  (new attribute
+     ProtocolInformationMatch.add
+     ProtocolInformationMatch.mem
+     ProtocolInformationMatch.remove
+     ProtocolInformationMatch.empty
+     ProtocolInformationMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.27 NAME 'generalizedTimeMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.24 *)
 let generalized_time_equality_match v1 v2 = String.compare v1 v2
@@ -165,14 +202,14 @@ module GeneralizedTimeMatch = Set.Make
      let compare = generalized_time_equality_match
    end)
 
-let generalized_time_equality_set = {
-  add=GeneralizedTimeMatch.add;
-  mem=GeneralizedTimeMatch.mem;
-  remove=GeneralizedTimeMatch.remove;
-  empty=GeneralizedTimeMatch.empty;
-  elements=GeneralizedTimeMatch.elements;
-  compare=generalized_time_equality_match
-}
+let new_generalized_time_equality_set syntax =
+  (new attribute
+     GeneralizedTimeMatch.add
+     GeneralizedTimeMatch.mem
+     GeneralizedTimeMatch.remove
+     GeneralizedTimeMatch.empty
+     GeneralizedTimeMatch.elements
+     syntax :> attribute_t)
 
 let whsp = Pcre.regexp ~study:true "\\s+"
 let leading_or_trailing_whsp = Pcre.regexp ~study:true "(^\\s+|\\s+$)"
@@ -192,14 +229,14 @@ module CaseIgnoreMatch = Set.Make
      let compare = case_ignore_equality_match
    end)
 
-let case_ignore_equality_set = {
-  add=CaseIgnoreMatch.add;
-  mem=CaseIgnoreMatch.mem;
-  remove=CaseIgnoreMatch.remove;
-  empty=CaseIgnoreMatch.empty;
-  elements=CaseIgnoreMatch.elements;
-  compare=case_ignore_equality_match
-}
+let new_case_ignore_equality_set syntax =
+  (new attribute
+     CaseIgnoreMatch.add
+     CaseIgnoreMatch.mem
+     CaseIgnoreMatch.remove
+     CaseIgnoreMatch.empty
+     CaseIgnoreMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.11 NAME 'caseIgnoreListMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.41 *)
 let case_ignore_list_equality_match = case_ignore_equality_match
@@ -210,14 +247,14 @@ module CaseIgnoreListMatch = Set.Make
      let compare = case_ignore_list_equality_match
    end)
 
-let case_ignore_list_equality_set = {
-  add=CaseIgnoreListMatch.add;
-  mem=CaseIgnoreListMatch.mem;
-  remove=CaseIgnoreListMatch.remove;
-  empty=CaseIgnoreListMatch.empty;
-  elements=CaseIgnoreListMatch.elements;
-  compare=case_ignore_list_equality_match
-}
+let new_case_ignore_list_equality_set syntax =
+  (new attribute
+     CaseIgnoreListMatch.add
+     CaseIgnoreListMatch.mem
+     CaseIgnoreListMatch.remove
+     CaseIgnoreListMatch.empty
+     CaseIgnoreListMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.20 NAME 'telephoneNumberMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.50 *)
 let telephone_number_equality_match v1 v2 = 
@@ -231,14 +268,14 @@ module TelephoneNumberMatch = Set.Make
      let compare = telephone_number_equality_match
    end)
 
-let telephone_number_equality_set = {
-  add=TelephoneNumberMatch.add;
-  mem=TelephoneNumberMatch.mem;
-  remove=TelephoneNumberMatch.remove;
-  empty=TelephoneNumberMatch.empty;
-  elements=TelephoneNumberMatch.elements;
-  compare=telephone_number_equality_match
-}
+let new_telephone_number_equality_set syntax =
+  (new attribute
+     TelephoneNumberMatch.add
+     TelephoneNumberMatch.mem
+     TelephoneNumberMatch.remove
+     TelephoneNumberMatch.empty
+     TelephoneNumberMatch.elements
+     syntax :> attribute_t)
 
 (* 1.3.6.1.4.1.1466.109.114.1 NAME 'caseExactIA5Match' SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 *)
 let case_exact_ia5_equality_match v1 v2 = 
@@ -252,14 +289,14 @@ module CaseExactIA5Match = Set.Make
      let compare = case_exact_ia5_equality_match
    end)
 
-let case_exact_ia5_equality_set = {
-  add=CaseExactIA5Match.add;
-  mem=CaseExactIA5Match.mem;
-  remove=CaseExactIA5Match.remove;
-  empty=CaseExactIA5Match.empty;
-  elements=CaseExactIA5Match.elements;
-  compare=case_exact_ia5_equality_match
-}
+let new_case_exact_ia5_equality_set syntax =
+  (new attribute
+     CaseExactIA5Match.add
+     CaseExactIA5Match.mem
+     CaseExactIA5Match.remove
+     CaseExactIA5Match.empty
+     CaseExactIA5Match.elements
+     syntax :> attribute_t)
 
 (* 1.3.6.1.4.1.1466.109.114.2 NAME 'caseIgnoreIA5Match' SYNTAX 1.3.6.1.4.1.1466.115.121.1.26 *)
 let case_ignore_ia5_equality_match v1 v2 = 
@@ -273,14 +310,14 @@ module CaseIgnoreIA5Match = Set.Make
      let compare = case_ignore_ia5_equality_match
    end)
 
-let case_ignore_ia5_equality_set = {
-  add=CaseIgnoreIA5Match.add;
-  mem=CaseIgnoreIA5Match.mem;
-  remove=CaseIgnoreIA5Match.remove;
-  empty=CaseIgnoreIA5Match.empty;
-  elements=CaseIgnoreIA5Match.elements;
-  compare=case_ignore_ia5_equality_match
-}
+let new_case_ignore_ia5_equality_set syntax =
+  (new attribute
+     CaseIgnoreIA5Match.add
+     CaseIgnoreIA5Match.mem
+     CaseIgnoreIA5Match.remove
+     CaseIgnoreIA5Match.empty
+     CaseIgnoreIA5Match.elements
+     syntax :> attribute_t)
 
 (* ordering matching rules used in inequality filters *)
 
@@ -293,14 +330,14 @@ module GeneralizedTimeOrderingMatch = Set.Make
      let compare = generalized_time_ordering_match
    end)
 
-let generalized_time_ordering_set = {
-  add=GeneralizedTimeOrderingMatch.add;
-  mem=GeneralizedTimeOrderingMatch.mem;
-  remove=GeneralizedTimeOrderingMatch.remove;
-  empty=GeneralizedTimeOrderingMatch.empty;
-  elements=GeneralizedTimeOrderingMatch.elements;
-  compare=generalized_time_ordering_match
-}
+let new_generalized_time_ordering_set syntax =
+  (new attribute
+     GeneralizedTimeOrderingMatch.add
+     GeneralizedTimeOrderingMatch.mem
+     GeneralizedTimeOrderingMatch.remove
+     GeneralizedTimeOrderingMatch.empty
+     GeneralizedTimeOrderingMatch.elements
+     syntax :> attribute_t)
 
 (* 2.5.13.3 NAME 'caseIgnoreOrderingMatch' SYNTAX 1.3.6.1.4.1.1466.115.121.1.15 *)
 let case_ignore_ordering_match v1 v2 = 
@@ -314,14 +351,14 @@ module CaseIgnoreOrderingMatch = Set.Make
      let compare = case_ignore_ordering_match
    end)
 
-let case_ignore_ordering_set = {
-  add=CaseIgnoreOrderingMatch.add;
-  mem=CaseIgnoreOrderingMatch.mem;
-  remove=CaseIgnoreOrderingMatch.remove;
-  empty=CaseIgnoreOrderingMatch.empty;
-  elements=CaseIgnoreOrderingMatch.elements;
-  compare=case_ignore_ordering_match
-}
+let new_case_ignore_ordering_set syntax =
+  (new attribute
+     CaseIgnoreOrderingMatch.add
+     CaseIgnoreOrderingMatch.mem
+     CaseIgnoreOrderingMatch.remove
+     CaseIgnoreOrderingMatch.empty
+     CaseIgnoreOrderingMatch.elements
+     syntax :> attribute_t)
 
 (* substring matching rules, these are different beasts *)
 
@@ -338,43 +375,43 @@ let equality_matching_rules =
   List.fold_left
     (fun m (key, value) -> Oidmap.add key value m)
     Oidmap.empty
-    [(Oid.of_string "2.5.13.0", object_identifier_equality_match);
-     (Oid.of_string "objectidentifiermatch", object_identifier_equality_match);
-     (Oid.of_string "2.5.13.1", distinguished_name_equality_match);
-     (Oid.of_string "distinguishednamematch", distinguished_name_equality_match);
-     (Oid.of_string "2.5.13.8", numeric_string_equality_match);
-     (Oid.of_string "numericstringmatch", numeric_string_equality_match);
-     (Oid.of_string "2.5.13.14", integer_equality_match);
-     (Oid.of_string "integermatch", integer_equality_match);
-     (Oid.of_string "2.5.13.16", bit_string_equality_match);
-     (Oid.of_string "bitstringmatch", bit_string_equality_match);
-     (Oid.of_string "2.5.13.22", presentation_address_equality_match);
-     (Oid.of_string "presentationaddressmatch", presentation_address_equality_match);
-     (Oid.of_string "2.5.13.23", unique_member_equality_match);
-     (Oid.of_string "uniquemembermatch", unique_member_equality_match);
-     (Oid.of_string "2.5.13.24", protocol_information_equality_match);
-     (Oid.of_string "protocolinformationmatch", protocol_information_equality_match);
-     (Oid.of_string "2.5.13.27", generalized_time_equality_match);
-     (Oid.of_string "generalizedtimematch", generalized_time_equality_match);
-     (Oid.of_string "2.5.13.2", case_ignore_equality_match);
-     (Oid.of_string "caseignorematch", case_ignore_equality_match);
-     (Oid.of_string "2.5.13.11", case_ignore_list_equality_match);
-     (Oid.of_string "caseignorelistmatch", case_ignore_list_equality_match);
-     (Oid.of_string "2.5.13.20", telephonenumber_equality_match);
-     (Oid.of_string "telephonenumbermatch", telephonenumber_equality_match);
-     (Oid.of_string "1.3.6.1.4.1.1466.109.114.1", case_exact_ia5_equality_match);
-     (Oid.of_string "caseexactia5match", case_exact_ia5_equality_match);
-     (Oid.of_string "1.3.6.1.4.1.1466.109.114.2", case_ignore_ia5_equality_match);
-     (Oid.of_string "caseignoreia5match", case_ignore_ia5_equality_match)]
+    [(Oid.of_string "2.5.13.0", new_object_identifier_equality_set);
+     (Oid.of_string "objectidentifiermatch", new_object_identifier_equality_set);
+     (Oid.of_string "2.5.13.1", new_distinguished_name_equality_set);
+     (Oid.of_string "distinguishednamematch", new_distinguished_name_equality_set);
+     (Oid.of_string "2.5.13.8", new_numeric_string_equality_set);
+     (Oid.of_string "numericstringmatch", new_numeric_string_equality_set);
+     (Oid.of_string "2.5.13.14", new_integer_equality_set);
+     (Oid.of_string "integermatch", new_integer_equality_set);
+     (Oid.of_string "2.5.13.16", new_bit_string_equality_set);
+     (Oid.of_string "bitstringmatch", new_bit_string_equality_set);
+     (Oid.of_string "2.5.13.22", new_presentation_address_equality_set);
+     (Oid.of_string "presentationaddressmatch", new_presentation_address_equality_set);
+     (Oid.of_string "2.5.13.23", new_unique_member_equality_set);
+     (Oid.of_string "uniquemembermatch", new_unique_member_equality_set);
+     (Oid.of_string "2.5.13.24", new_protocol_information_equality_set);
+     (Oid.of_string "protocolinformationmatch", new_protocol_information_equality_set);
+     (Oid.of_string "2.5.13.27", new_generalized_time_equality_set);
+     (Oid.of_string "generalizedtimematch", new_generalized_time_equality_set);
+     (Oid.of_string "2.5.13.2", new_case_ignore_equality_set);
+     (Oid.of_string "caseignorematch", new_case_ignore_equality_set);
+     (Oid.of_string "2.5.13.11", new_case_ignore_list_equality_set);
+     (Oid.of_string "caseignorelistmatch", new_case_ignore_list_equality_set);
+     (Oid.of_string "2.5.13.20", new_telephone_number_equality_set);
+     (Oid.of_string "telephonenumbermatch", new_telephone_number_equality_set);
+     (Oid.of_string "1.3.6.1.4.1.1466.109.114.1", new_case_exact_ia5_equality_set);
+     (Oid.of_string "caseexactia5match", new_case_exact_ia5_equality_set);
+     (Oid.of_string "1.3.6.1.4.1.1466.109.114.2", new_case_ignore_ia5_equality_set);
+     (Oid.of_string "caseignoreia5match", new_case_ignore_ia5_equality_set)]
 
 let ordering_matching_rules =
   List.fold_left
     (fun m (key, value) -> Oidmap.add key value m)
     Oidmap.empty
-    [(Oid.of_string "2.5.13.28", generalized_time_ordering_match);
-     (Oid.of_string "generalizedtimeorderingmatch", generalized_time_ordering_match);
-     (Oid.of_string "2.5.13.3", case_ignore_ordering_match);
-     (Oid.of_string "caseignoreorderingmatch", case_ignore_ordering_match)]
+    [(Oid.of_string "2.5.13.28", generalized_time_ordering_set);
+     (Oid.of_string "generalizedtimeorderingmatch", generalized_time_ordering_set);
+     (Oid.of_string "2.5.13.3", case_ignore_ordering_set);
+     (Oid.of_string "caseignoreorderingmatch", case_ignore_ordering_set)]
 
 let substring_matching_rules = 
   List.fold_left
