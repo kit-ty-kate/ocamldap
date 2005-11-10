05841d5256e4df143f3a56fa89d7343b3dfe509c
Eric Stokes <letaris@me.com>
2005-11-10 22:38:39 +0000

    better types in the schema validator, differentiation between objectclasses and attributes in the name field (an important feature for an automated repair algorythm)

--- ldap_schemavalidator.ml
+++ ldap_schemavalidator.ml
@@ -2,12 +2,16 @@ open Ldap_schema
 open Ldap_matchingrules
 open Ldap_syntaxes
 
+type schema_object =
+    [ `Attribute of string
+    | `Objectclass of string ] 
+
 type schema_error = 
     Non_unique_name_for_must_or_may of string
   | Undefined_must_or_may of string
   | Non_unique_name_for_superior_oc of string
   | Undefined_superior_oc of string
-  | Cross_linked_oid of string list
+  | Cross_linked_oid of schema_object list
   | Unknown_syntax of Oid.t
   | Unknown_matching_rule of Oid.t
   | No_equality_matching_rule_defined
@@ -22,9 +26,9 @@ let validate schema =
 	   try ignore (attrNameToAttr schema attr);errors
 	   with 
 	       Invalid_attribute _ -> 
-		 (oc, Undefined_must_or_may attr) :: errors
+		 (`Objectclass oc, Undefined_must_or_may attr) :: errors
 	     | Non_unique_attribute_alias attr -> 
-		 (oc, Non_unique_name_for_must_or_may attr) :: errors
+		 (`Objectclass oc, Non_unique_name_for_must_or_may attr) :: errors
 	 in
 	   (List.rev_append errors
 	      (List.rev_append
@@ -33,18 +37,18 @@ let validate schema =
       schema.objectclasses
       []
   in
-  (* check for cross linked oids *)
+    (* check for cross linked oids *)
   let errors =
     let oids = Hashtbl.create 100 in
     let seen = Hashtbl.create 100 in
       Hashtbl.iter
-	(fun oid {at_name=n} -> Hashtbl.add oids oid (List.hd n))
+	(fun oid {at_name=n} -> Hashtbl.add oids oid (`Attribute (List.hd n)))
 	schema.attributes_byoid;
       Hashtbl.iter
-	(fun oid {oc_name=n} -> Hashtbl.add oids oid (List.hd n))
+	(fun oid {oc_name=n} -> Hashtbl.add oids oid (`Objectclass (List.hd n)))
 	schema.objectclasses_byoid;
       Hashtbl.fold
-	(fun oid (name: string) errors ->
+	(fun oid name errors ->
 	   if List.length (Hashtbl.find_all oids oid) > 1 then
 	     if Hashtbl.mem seen oid then errors
 	     else begin
@@ -55,51 +59,52 @@ let validate schema =
 	     errors)
 	oids
 	errors
-  in
-  (* make sure all superior ocs are defined *)
-  let errors =
-    Hashtbl.fold
-      (fun oc {oc_sup=sups} errors ->
-	 let oc = Lcstring.to_string oc in
-	   List.fold_left
-	     (fun errors sup -> 
-		try ignore (ocNameToOc schema sup);errors
-		with 
-		    Invalid_objectclass _ -> 
-		      (oc, Undefined_superior_oc sup) :: errors
-		  | Non_unique_objectclass_alias _ ->
-		      (oc, Non_unique_name_for_superior_oc sup) :: errors)
-	     errors
-	     sups)
-      schema.objectclasses
+    in
+      (* make sure all superior ocs are defined *)
+    let errors =
+      Hashtbl.fold
+	(fun oc {oc_sup=sups} errors ->
+	   let oc = Lcstring.to_string oc in
+	     List.fold_left
+	       (fun errors sup -> 
+		  try ignore (ocNameToOc schema sup);errors
+		  with 
+		      Invalid_objectclass _ -> 
+			(`Objectclass oc, Undefined_superior_oc sup) :: errors
+		    | Non_unique_objectclass_alias _ ->
+			(`Objectclass oc, Non_unique_name_for_superior_oc sup) :: errors)
+	       errors
+	       sups)
+	schema.objectclasses
+	errors
+    in
+      (* check that we know all of the syntaxes defined in the schema *)
+    let errors = 
+      Hashtbl.fold
+	(fun attr {at_syntax=syntax} errors ->
+	   let attr = Lcstring.to_string attr in
+	     if Oidmap.mem syntax Ldap_syntaxes.syntaxes then errors
+	     else (`Attribute attr, Unknown_syntax syntax) :: errors)
+	schema.attributes
+	errors
+    in
+      (* check that an equality matching rule is defined either directly,
+	 indirectly by a parent attribute, or by virtue of the syntax of
+	 the attribute *)
+    let errors = 
+      Hashtbl.fold
+	(fun attrname attr errors ->
+	   let attrname = Lcstring.to_string attrname in
+	     try
+	       begin match lookupMatchingRule schema `Equality attr with
+		   Some mrule -> errors
+		 | None -> (* try it by syntax *) 
+		     if Oidmap.mem attr.at_syntax Ldap_matchingrules.equality_bysyntax then errors
+		     else (`Attribute attrname, No_equality_matching_rule_defined) :: errors
+	       end
+	     with _ -> (`Attribute attrname, No_equality_matching_rule_defined) :: errors)
+	schema.attributes
+	errors
+    in
       errors
-  in
-  (* check that we know all of the syntaxes defined in the schema *)
-  let errors = 
-    Hashtbl.fold
-      (fun attr {at_syntax=syntax} errors ->
-	 let attr = Lcstring.to_string attr in
-	   if Oidmap.mem syntax Ldap_syntaxes.syntaxes then errors
-	   else (attr, Unknown_syntax syntax) :: errors)
-      schema.attributes
-      errors
-  in
-  (* check that an equality matching rule is defined either directly,
-     indirectly by a parent attribute, or by virtue of the syntax of
-     the attribute *)
-  let errors = 
-    Hashtbl.fold
-      (fun attrname attr errors ->
-	 let attrname = Lcstring.to_string attrname in
-	   try
-	     begin match lookupMatchingRule schema `Equality attr with
-		 Some mrule -> errors
-	       | None -> (* try it by syntax *) 
-		   if Oidmap.mem attr.at_syntax Ldap_matchingrules.equality_bysyntax then errors
-		   else (attrname, No_equality_matching_rule_defined) :: errors
-	     end
-	   with _ -> (attrname, No_equality_matching_rule_defined) :: errors)
-      schema.attributes
-      errors
-  in
-    errors
+;;
